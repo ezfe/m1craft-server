c505644b869cb0efcf59a260eb982287d96ff887
Ezekiel Elin <ezekielelin@me.com>
2022-09-20 21:43:14 -0400

    Update index page

--- Package.swift
+++ Package.swift
@@ -2,39 +2,41 @@
 import PackageDescription
 
 let package = Package(
-    name: "m1craft-server",
-    platforms: [
-       .macOS(.v12)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/ezfe/minecraft-jar-command.git", branch: "main"),
-//        .package(path: "/Users/ezekielelin/github_repositories/Minecraft Launcher/minecraft-jar-command"),
-        .package(url: "https://github.com/mxcl/Version.git", from: "2.0.1")
-    ],
-    targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor"),
-					 .product(name: "Common", package: "minecraft-jar-command"),
-					 .product(name: "InstallationManager", package: "minecraft-jar-command"),
-                .product(name: "Version", package: "Version")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
-    ]
+	name: "m1craft-server",
+	platforms: [
+		.macOS(.v12)
+	],
+	dependencies: [
+		// 💧 A server-side Swift web framework.
+		.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+		.package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+		.package(url: "https://github.com/ezfe/minecraft-jar-command.git", branch: "main"),
+		// .package(path: "/Users/ezekielelin/github_repositories/Minecraft Launcher/minecraft-jar-command"),
+		.package(url: "https://github.com/mxcl/Version.git", from: "2.0.1"),
+		.package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.2.0")
+	],
+	targets: [
+		.target(
+			name: "App",
+			dependencies: [
+				.product(name: "Leaf", package: "leaf"),
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "Common", package: "minecraft-jar-command"),
+				.product(name: "InstallationManager", package: "minecraft-jar-command"),
+				.product(name: "Version", package: "Version"),
+				.product(name: "Regex", package: "Regex")
+			],
+			swiftSettings: [
+				// Enable better optimizations when building in Release configuration. Despite the use of
+				// the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+				// builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
+				.unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+			]
+		),
+		.executableTarget(name: "Run", dependencies: [.target(name: "App")]),
+		.testTarget(name: "AppTests", dependencies: [
+			.target(name: "App"),
+			.product(name: "XCTVapor", package: "vapor"),
+		])
+	]
 )
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,42 @@
-import Vapor
+import Common
 import Foundation
 import InstallationManager
+import Regex
+import Vapor
+
+struct IndexContext: Encodable {
+	let version: String?
+	let size: Double?
+	let url: String?
+}
 
 func routes(_ app: Application) throws {
-	app.get { req in
-		return req.view.render("index", ["title": "Hello Vapor!"])
-	}
-	
-	app.get("hello") { req -> String in
-		return "Hello, world!"
+	app.get() { (req: Request) async throws -> Response in
+		let appcastUrl = URL(string: "https://f001.backblazeb2.com/file/minecraft-jar-command/appcast/appcast")!
+		let appcastResponse = try await retrieveData(from: appcastUrl).0
+		let appcastXml = String(data: appcastResponse, encoding: .utf8)!
+		
+		let versionRegex = #"<sparkle:shortVersionString>(.+)</sparkle:shortVersionString>"#.r!
+		let downloadRegex = #"<enclosure url="(.+)" length="(\d+)""#.r!
+		
+		let version: String? = versionRegex.findFirst(in: appcastXml)?.group(at: 1)
+		let url: String? = downloadRegex.findFirst(in: appcastXml)?.group(at: 1)
+		var size: Double? = nil
+		
+		if let sizeStr = downloadRegex.findFirst(in: appcastXml)?.group(at: 2),
+			let sizeBytes = Double(sizeStr) {
+			
+			size = round(sizeBytes / 100_000.0) / 10.0
+		}
+		
+		let view: View = try await req.view.render(
+			"index",
+			IndexContext(version: version, size: size, url: url)
+		)
+		let response = try await view.encodeResponse(for: req)
+		response.headers.add(name: .cacheControl, value: "public, max-age=3600")
+		return response
+		
 	}
 	
 	try app.register(collection: ApiController())
